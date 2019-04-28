acffc0c10c36c17ddbfc53abb11d86c093c24acc
Justine Joshua Quiazon <jio_qzn@yahoo.com>
2019-04-28 22:13:23 +0800

    v1.0.3 (#4)
    * Removed web-gl and vue-typer
    - Removed isMobile package
    - Code clean up
    - Updated and Fixed packages
    - Updated readme Includes section
    
    * Updated PWA app icons
    - Updated manifest.json icons
    - Added online resources section in readme

--- src/components/filler.vue
+++ src/components/filler.vue
@@ -1,46 +1,6 @@
 <template>
     <section>
-        <!-- For desktop -->
-        <v-parallax 
-            v-if="!isMobile" 
-            :src="require('@/assets/images/filler-blurred.jpg')" 
-            height="450" 
-            alt="filler"
-        >
-            <v-layout 
-                row wrap 
-                align-center 
-                justify-center 
-                class="has-elevation"
-            >
-                <v-flex xl6 lg6 md8 offset-xs-3>
-                    <blockquote class="title white--text mb-3 text-xs-center">
-                        <v-icon class="white--text">format_quote</v-icon>
-                        {{ quote }}
-                        <v-icon class="white--text">format_quote</v-icon>
-                    </blockquote>
-                    <h4 class="subheading orange--text text--lighten-1 text-xs-center">- {{ quoteBy }}</h4>
-                    <div 
-                        class="text-xs-center" 
-                        v-for="(item, index) in titles" 
-                        :key="index"
-                    >
-                        {{ item.name }}
-                    </div>
-                </v-flex>
-            </v-layout>
-
-            <vue-particles 
-                color="#FF9800"
-                shapeType="polygon"
-                :clickEffect="false" 
-                :linesDistance=90
-            ></vue-particles>
-        </v-parallax>
-
-        <!-- For mobile -->
         <v-img
-            v-else
             :src="require('@/assets/images/filler-blurred.jpg')"
             :lazy-src="require('@/assets/images/place-holder.jpg')"
             height="450"
@@ -66,13 +26,10 @@
                 </v-flex>
             </v-layout>
         </v-img>
-
     </section>
 </template>
 
 <script>
-import isMobile from 'ismobilejs'
-
 export default {
     data () {
         return {
@@ -84,24 +41,9 @@ export default {
                 { name: 'creator of JSLint' }
             ]
         }
-    },
-
-    computed: {
-        isMobile () {
-            return isMobile.any
-        }
     }
 }
 </script>
-
-<style scoped>
-    #particles-js {
-        position: absolute;
-        width: 100%;
-        height: 100vh;
-        right: 3px;
-    }
-</style>
 
 
 
--- src/main.js
+++ src/main.js
@@ -3,16 +3,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import Meta from 'vue-meta'
-import VueTyperPlugin from 'vue-typer'
 import VueClipboard from 'vue-clipboard2'
-import VueParticles from 'vue-particles'
 import './plugins/vuetify'
 import './assets/css/style.css'
 import './registerServiceWorker'
 
 Vue.use(Meta)
-Vue.use(VueTyperPlugin)
-Vue.use(VueParticles)
 Vue.use(VueClipboard)
 
 Vue.config.productionTip = false
